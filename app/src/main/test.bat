80fb9a182f2224dbc83850232628c08b2f88ef5e
zlw <zlw@xdja.com>
2016-12-08 17:34:54 +0800

    修改了编译错误

--- app/src/main/test.bat
+++
@@ -1,9 +0,0 @@
-set NODE=versionNumber
-set XML=AndroidManifest.xml
-for /f "tokens=2 delims=>" %%i in ('findstr "<%NODE%>" %XML%') do (
-    for /f "delims=<" %%i in ("%%i")do (
-        echo %%i
-    )
-)
-
-pause
